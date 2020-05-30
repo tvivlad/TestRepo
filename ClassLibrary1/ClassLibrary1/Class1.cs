3ec8276ef9c7932ef96f0010131daede0d92b38e
vlad <35404251+tvivlad@users.noreply.github.com>
2020-05-30 08:41:09 +0500

    CalcChange

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calc
+{
+    public class Calcul
+    {
+        public int sum(int x, int y)
+        {
+            return x + y; 
+        }
+        public int dif(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int mult(int x, int y)
+        {
+            return x * y; 
+        }
+    }
+}
